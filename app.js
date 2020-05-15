3451b7fb4418e3d0223d4eb59e1a4048b96cec55
DEEPAK-crypto <deepsdeep45@gmail.com>
2020-05-15 18:47:42 +0530

    added friends array

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ var app = express();
 
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res) {
+    res.render("home");
+
+});
+app.get("/friends", function(req, res) {
+    var friends = ["Shantanu", 'Rohan', 'Surya', 'Agrim', 'Dev'];
+    res.render("friends", { friends: friends });
+
+});
 app.listen(3000);
